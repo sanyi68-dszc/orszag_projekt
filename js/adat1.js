f9909b4b032c14960b199dc1076093524856beef
Sajat Nev <email@pelda.hu>
2026-04-29 11:30:14 +0200

    Nevezetességek alap

--- js/adat1.js
+++ js/adat1.js
@@ -1,17 +1,17 @@
 termeszetTomb=[
     {
-        "nev":"Balti-tenger",
-        "leiras":"A Balti-tenger vagy Keleti-tenger Észak-Európában található tenger, északról a Skandináv-félsziget, keletről és délről az európai kontinens, nyugatról a dán szigetek határolják. A Balti-tenger három dán tengerszoroson kapcsolódik az Északi-tengeren keresztül az Atlanti-óceánhoz: Øresund, Nagy-Bælt és Kis-Bælt.",
-        "kep":"balti-tenger.jpg"
+        "nev":"Helsinki",
+        "leiras":"Finnország fővárosa és egyben legnagyobb városa. Az európai kontinens legészakibb fővárosa. Az ország déli részén, a Finn-öböl partján helyezkedik el. 635 591 ember lakja, ezzel messze az ország legnagyobb városa.",
+        "kep":"helsinki.jpg"
     },
     {
-        "nev":"placeholder2",
-        "leiras":"placeholder2",
-        "kep":"placeholder2.jpg"
+        "nev":"Rovaniemi",
+        "leiras":"Az Északi-sarkkörön fekvő város Lappföldön található, amely a Mikulás hivatalos otthonaként is ismert. A város központjától nyolc kilométernyire található Santa Parkban találkozhatunk a Mikulással és akár postázhatjuk a karácsonyi üdvözlőlapjaikat a Mikulás hivatalos postahivatalából.",
+        "kep":"rovaniemi.jpg"
     },
     {
-        "nev":"placeholder3",
-        "leiras":"placeholder3",
-        "kep":"placeholder3.jpg"
+        "nev":"Lemmenjoki Nemzeti Park",
+        "leiras":"Finnország legnagyobb nemzeti parkja több mint 2850 négyzetkilométer és Európa egyik legnagyobb érintetlen vadonja. Nevét a Lemmenjoki folyóról kapta, amely átszeli a területet, és fontos élőhelyet biztosít sok vadon élő állatfaj számára. A park táját változatos domborzati formák, hatalmas erdőségek, tágas mocsarak, valamint számos tó és folyó jellemzi. Különleges vonzereje a lapp kultúra és a hagyományos sami életmód. A samik, Európa legészakibb őslakos népe, hosszú ideje használják ezt a területet rénszarvasok legeltetésére.",
+        "kep":"lemmenjoki-national-park.jpg"
     }
 ]
