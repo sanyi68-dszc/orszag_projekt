5ce50bf814273008e7023952b8f287d1eab5c816
Sajat Nev <email@pelda.hu>
2026-05-12 08:14:03 +0200

    Apróbb javítások

--- js/galleria.js
+++ js/galleria.js
@@ -1,8 +1,8 @@
 kepek=""
 for (let i = 1; i <= 12; i++) {
     kepek+=`
-    <div class="col-sm-3">
-            <img style="margin-top: 6px; margin-bottom: 6px" src="kepek/szauna${i}.jpg" width="300px">
+    <div class="col-sm-3 text-center">
+        <p><img style="margin-top: 6px; margin-bottom: 6px; padding: 6px" src="kepek/szauna${i}.jpg" width="300px"></p>
     </div>
     `
 }
